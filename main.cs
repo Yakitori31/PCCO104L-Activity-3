8b70a8038eccee4f98d98541a3d5a0e24625776f
Yakitori31 <158657248+Yakitori31@users.noreply.github.com>
2024-02-24 03:23:59 +0000

    Try

--- main.cs
+++ main.cs
@@ -1,7 +1,49 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    static void Main()
+    {
+        int number;
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out number) && number >= 0)
+            {
+                if (number == 0)
+                {
+                    Console.WriteLine("Closing Program...");
+                    break;
+                }
+
+                PrintAsteriskPyramid(number);
+            }
+            else
+            {
+                Console.WriteLine($"Invalid value: {input}");
+            }
+
+        } while (true);
+    }
+
+    static void PrintAsteriskPyramid(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 1; k <= 2 * i - 1; k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
 }
